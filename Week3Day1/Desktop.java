7e3c814379a345c88da92bab47e8a654d7e1bd35
AnithaDevi-26 <129593091+AnithaDevi-26@users.noreply.github.com>
2023-04-22 18:20:13 +0530

    Add files via upload

---
+++ Week3Day1/Desktop.java
@@ -0,0 +1,16 @@
+package System;
+
+public class Desktop extends Computera{
+
+	public void desktopSize() {
+		System.out.println("From Desktop");
+		
+	}
+	public static void main(String[] args) {
+		
+		Desktop desktop = new Desktop();
+		desktop.desktopSize();
+		desktop.computerModel();
+		
+	}
+}
