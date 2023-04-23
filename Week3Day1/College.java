9d8b5be4cdc14a167420bedea36ae265812cf3f6
AnithaDevi-26 <129593091+AnithaDevi-26@users.noreply.github.com>
2023-04-23 13:32:21 +0530

    Add files via upload

---
+++ Week3Day1/College.java
@@ -0,0 +1,15 @@
+package College;
+
+public class College {
+	public void collegeName() {
+		System.out.println("collegename from College");
+	}
+	public void collegeCode(){
+		System.out.println("collegeCode from College");
+		}
+	
+	public void collegeRank() {
+		System.out.println("collegeRank from College");
+	}
+
+}
